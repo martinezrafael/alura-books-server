3dad892c9caaa5b78079b951e03716b6147d9e79
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-29 15:41:58 -0300

    adiciona o trycath na requisicao

--- routes/livrosRoutes.js
+++ routes/livrosRoutes.js
@@ -3,7 +3,16 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({ messsage: "Hello World!" });
+  try {
+    res.status(200).json({ message: "Deu tudo certo!" });
+  } catch (error) {
+    res.status(500).json({ message: `Erroou! ${error.message}` });
+  }
+});
+
+router.post("/", (req, res) => {
+  const livroCriado = req.body;
+  res.status(200).json(livroCriado);
 });
 
 export default router;
