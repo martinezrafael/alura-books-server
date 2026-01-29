aff1522332b7f82c1e51de8d45fc579c8c139b8e
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-29 16:51:20 -0300

    cria a rota de POST

--- controllers/livrosController.js
+++ controllers/livrosController.js
@@ -1,9 +1,26 @@
+import fs from "fs";
+
 const getLivros = (req, res) => {
+  const livros = fs.readFileSync("./data/livros.json");
   try {
-    res.status(200).json({ message: "Deu tudo certo" });
+    res.status(200).json(JSON.parse(livros));
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export default getLivros;
+const createLivro = (req, res) => {
+  try {
+    const database = JSON.parse(fs.readFileSync("./data/livros.json"));
+    const livroCriado = req.body;
+    fs.writeFileSync(
+      "./data/livros.json",
+      JSON.stringify([...database, livroCriado]),
+    );
+    res.status(200).json(livroCriado);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { getLivros, createLivro };
--- routes/livrosRoutes.js
+++ routes/livrosRoutes.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import getLivros from "../controllers/livrosController.js";
+import { getLivros, createLivro } from "../controllers/livrosController.js";
 
 const router = Router();
 
 router.get("/", getLivros);
 
-router.post("/", (req, res) => {
-  const livroCriado = req.body;
-  res.status(200).json(livroCriado);
-});
+router.post("/", createLivro);
 
 export default router;
