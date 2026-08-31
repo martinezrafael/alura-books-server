8b357759d0535e033438fe8e2b0bffc7cc0b9172
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-31 16:12:02 -0300

    passa para o controlador a responsabilidade de ler o arquivo json

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,6 +1,9 @@
+const fs = require("fs");
+
 function getLivros(req, res) {
   try {
-    res.send("Olá mundo da Alura do Brasil");
+    const livros = JSON.parse(fs.readFileSync("livros.json"));
+    res.send(livros);
   } catch (error) {
     res.status(500);
     res.send(error.message);
