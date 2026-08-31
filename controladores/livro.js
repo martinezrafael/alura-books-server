5930aee50a5f31f93f0f8773f08dcd4c77a57b21
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-31 15:22:42 -0300

    cria rotas e controller

---
+++ controladores/livro.js
@@ -0,0 +1,12 @@
+function getLivros(req, res) {
+  try {
+    res.send("Olá mundo da Alura do Brasil");
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+}
+
+module.exports = {
+  getLivros,
+};
--- rotas/livro.js
+++ rotas/livro.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
+const { getLivros } = require("../controladores/livro");
 
 router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello world Alura");
+router.get("/", getLivros);
+
+router.post("/", (req, res) => {
+  res.send("Você fez uma requisição do tipo POST.");
+});
+
+router.patch("/", (req, res) => {
+  res.send("Você fez uma requisição do tipo PATCH.");
+});
+
+router.delete("/", (req, res) => {
+  res.send("Você fez uma requisição do tipo DELETE.");
 });
 
 module.exports = router;
